7b67fced7d9d775b9bb90e8c2e31433b68a2511f
louisdubruel <105044986+louisdubruel@users.noreply.github.com>
2023-02-21 16:35:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,60 @@
 from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+# import plotly.express as px
 
 # Load some sample data
 data = pd.DataFrame({
     'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
     'age': [25, 32, 18, 47, 29],
     'income': [50000, 70000, 30000, 90000, 60000],
+    'gender': ['F', 'M', 'M', 'M', 'F'],
+    'city': ['New York', 'San Francisco', 'New York', 'Boston', 'Chicago']
 })
 
-# Set page title
-st.set_page_config(page_title='My Streamlit Dashboard')
+# Set page title and favicon
+st.set_page_config(
+    page_title="Streamlit Dashboard",
+    page_icon=":bar_chart:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 # Add a title
 st.title('My Streamlit Dashboard')
 
-# Add a table of the data
-st.write(data)
+# Add a sidebar for filtering data
+st.sidebar.subheader('Filter Data')
+min_age, max_age = st.sidebar.slider('Select age range', 18, 60, (25, 50))
+selected_cities = st.sidebar.multiselect('Select cities', data['city'].unique(), default=['New York'])
 
-# Add a histogram of the ages
-fig = px.histogram(data, x='age')
+# Apply filters to data
+filtered_data = data[(data['age'] >= min_age) & (data['age'] <= max_age) & (data['city'].isin(selected_cities))]
+
+# Add a bar chart of income by gender
+st.header('Income by Gender')
+gender_income = filtered_data.groupby('gender')['income'].mean()
+st.bar_chart(gender_income)
+
+# Add a line chart of income by age
+st.header('Income by Age')
+age_income = filtered_data.groupby('age')['income'].mean()
+fig = px.line(x=age_income.index, y=age_income.values, labels={'x': 'Age', 'y': 'Income'})
 st.plotly_chart(fig)
+
+# Add a table of the filtered data
+st.header('Filtered Data')
+st.write(filtered_data)
+
+# Add responsiveness for different screen sizes
+col1, col2 = st.beta_columns([2, 1])
+with col1:
+    st.write('This is in the left column')
+with col2:
+    st.write('This is in the right column')
+
 
 
 """
